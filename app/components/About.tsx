fb3dda1aa83ab28cf2cef8fbbec6a0e4b63042df
albertomdg <albertodiaz9@protonmail.com>
2022-11-22 21:19:24 -0500

    first commit

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -9,15 +9,13 @@ const About = () => {
         <div className="ml-6">
           <h2 className="text-4xl font-bold mt-6">En Polimar</h2>
           <p className="mt-6 text-base font-normal">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-            volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
-            velit esse molestie consequat, vel illum dolore eu feugiat nulla
-            facilisis at vero eros et accumsan et iusto odio dignissim qui
-            blandit praesent luptatum zzril delenit augue duis dolore te feugait
-            nulla facilisi.
+            Nos comprometemos a suministrar empaques y envases plásticos
+            flexibles, así como insumos encaminados en la satisfacción total de
+            las necesidades de nuestros clientes, así como la búsqueda de la
+            mejora continua y la eficiencia del sistema de gestión de calidad y
+            sus procesos, contando con personal calificado y comprometido con la
+            organización, asegurando así una completa satisfacción de Nuestros
+            Clientes y nuestro reconocimiento como Empresa Líder.
           </p>
           <div className="mt-20">
             <Link className="bg-cyan-200 p-4 mt-16 text-white" href="/contacto">
