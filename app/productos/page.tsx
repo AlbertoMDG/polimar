2a345977da9a4ea7b541b76a32f2163cbd024ed9
albertomdg <albertodiaz9@protonmail.com>
2022-12-15 14:33:32 -0500

    modificacion de productos

--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -3,12 +3,30 @@ import Producto from "../components/Producto";
 export default function Productos() {
   return (
     <>
-      <Producto img="/bolsanegra.png" producto="Bolsa Negra" bg={true} />
+      <Producto
+        img="/bolsanegra.png"
+        producto="Bolsas de baja densidad tipo empaque y basura."
+        bg={true}
+      />
       <Producto img="/strech.png" producto="Strech" order={true} />
-      <Producto img="/camiseta.png" producto="Bolsa tipo camiseta" bg={true} />
+      <Producto
+        img="/camiseta.png"
+        producto="Bolsas de alta densidad tipo camiseta"
+        bg={true}
+      />
       <Producto
         img="/polietileno.png"
-        producto="Polietilono de alta densidad"
+        producto="Polietileno  de alta densidad (PEAD)"
+        order={true}
+      />
+      <Producto
+        img="/polibaja.png"
+        producto="Polietileno de Baja Densidad (PEBD)"
+        bg={true}
+      />
+      <Producto
+        img="/empaquesAuto.png"
+        producto="Bolsas para empacado automatico"
         order={true}
       />
     </>
